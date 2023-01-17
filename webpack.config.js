37fc8aabc3588a5e531147a1294f3c98e54199ea
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-01-17 09:12:04 +0100

    Added and changed webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.txt$/i,
+        type: "asset/source",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.png$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
+
+  
